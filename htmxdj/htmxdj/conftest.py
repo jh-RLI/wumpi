81fe2ac0264a4d85758de5d26934fe4b6478ec69
jh-RLI <jonas.huber@rl-institut.de>
2023-09-20 09:57:11 +0200

    imporve folder structure

---
+++ htmxdj/htmxdj/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from htmxdj.users.models import User
+from htmxdj.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
